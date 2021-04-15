f55c413c74c52874024539a4606f64742e43a09d
Igor Trifonov <Ha1lowe3n@yandex.ru>
2021-04-15 12:31:29 +0800

    lesson 13

--- src/stories/input.stories.tsx
+++ src/stories/input.stories.tsx
@@ -1,10 +1,12 @@
 import React, { ChangeEvent, useRef, useState } from "react";
+import { action } from "@storybook/addon-actions";
 
 export default {
     title: "input",
 };
 
 export const ControlledInputWithFixedValue = () => <input value={"hello"} />;
+
 export const UncontrolledInput = () => <input />;
 
 export const TrackValueOfUncontrolledInput = () => {
@@ -35,5 +37,46 @@ export const GetValueOfUncontrolledInputByButtonPress = () => {
             <input ref={inputRef} /> <button onClick={callback}>save</button> -
             actual value: {value}
         </>
+    );
+};
+
+export const ControlledInput = () => {
+    const [parentValue, setParentValue] = useState("");
+
+    const callback = (e: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(e.currentTarget.value);
+    };
+
+    return <input value={parentValue} onChange={callback} />;
+};
+
+export const ControlledCheckbox = () => {
+    const [parentValue, setParentValue] = useState(true);
+
+    const callback = (e: ChangeEvent<HTMLInputElement>) => {
+        setParentValue(e.currentTarget.checked);
+    };
+
+    return (
+        <input type={"checkbox"} checked={parentValue} onChange={callback} />
+    );
+};
+
+export const ControlledSelect = () => {
+    const [parentValue, setParentValue] = useState<string | undefined>(
+        undefined
+    );
+
+    const callback = (e: ChangeEvent<HTMLSelectElement>) => {
+        setParentValue(e.currentTarget.value);
+    };
+
+    return (
+        <select value={parentValue} onChange={callback}>
+            <option>none</option>
+            <option value={"1"}>Minsk</option>
+            <option value={"2"}>Moscow</option>
+            <option value={"3"}>Kiev</option>
+        </select>
     );
 };
